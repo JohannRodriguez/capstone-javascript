f6f183a30705c40abf07c5cbbdb1cff0daef3e5b
Lilith <themrblacksapce@hotmail.com>
2020-11-16 13:54:55 -0700

    Project setup with phaser, webpack and configure server to local and web deploy

---
+++ src/Scenes/GameScene.js
@@ -0,0 +1,20 @@
+import 'phaser';
+
+export default class GameScene extends Phaser.Scene {
+  constructor () {
+    super('Game');
+  }
+
+  preload () {
+  }
+
+  create () {
+    
+  }
+
+  update () {
+   
+  }
+
+  
+};
