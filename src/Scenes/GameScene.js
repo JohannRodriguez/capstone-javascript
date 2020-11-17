0944f0a115d1a5ae1f59b6509de0d54496de756f
Lilith <themrblacksapce@hotmail.com>
2020-11-17 13:08:42 -0700

    Create game logic basis

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,19 +1,20 @@
 import 'phaser';
+import config from '../Config/config';
 
 export default class GameScene extends Phaser.Scene {
   constructor () {
     super('Game');
   }
 
-  preload () {
-  }
-
   create () {
-    
+    this.obs = this.physics.add.image(config.width/2, config.height/2, 'checkedBox');
+    this.obs.setVelocityX(-200);
   }
 
   update () {
-   
+   if (this.obs.x < 20) {
+     this.obs.setX(config.width + 100);
+   }
   }
 
   
